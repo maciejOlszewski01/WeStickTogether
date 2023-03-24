8fb54f3cd57930685909d1496b27b47d76e0973a
Maciej Olszewski <maciej.olszewski01@gmail.com>
2023-03-24 21:18:15 +0100

    menu
    Poprawione menu

--- We Stick Together/Assets/Scripts/Network/NetworkManagerUI.cs
+++ We Stick Together/Assets/Scripts/Network/NetworkManagerUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
@@ -9,20 +10,29 @@ public class NetworkManagerUI : MonoBehaviour
 
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
-
+    public int gameStartScreen;
+    public static int flagH = 0;
+    public static int flagJ = 0;
     private void Awake()
     {
         hostButton.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.StartHost();
+            flagH = 1;
+            SceneManager.LoadScene(gameStartScreen);
+            /*NetworkManager.Singleton.StartHost();
             hostButton.gameObject.SetActive(false);
-            clientButton.gameObject.SetActive(false);
+            clientButton.gameObject.SetActive(false);*/
         });
         clientButton.onClick.AddListener(() =>
         {
+
+            flagJ = 1;
+            SceneManager.LoadScene(gameStartScreen);
+            /*
             NetworkManager.Singleton.StartClient();
             hostButton.gameObject.SetActive(false);
             clientButton.gameObject.SetActive(false);
+            */
         });
     }
 }
